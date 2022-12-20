89279d35f0b987a042fbdf259496e2435a84ddf1
duc-canh <canhndth2104014@fpt.edu.vn>
2022-12-20 18:52:26 +0700

    update starup

--- NetCore-Backend/Startup.cs
+++ NetCore-Backend/Startup.cs
@@ -30,11 +30,17 @@ namespace NetCore_Backend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
+
             app.UseAuthorization();
-            app.MapRazorPages();
+
+            app.MapControllers();
+
             app.Run();
         }
     }
