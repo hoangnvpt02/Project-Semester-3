d19d5635538d3451ad22fc8732c3e2ab7a196fae
duc-canh <canhndth2104014@fpt.edu.vn>
2022-12-24 15:44:44 +0700

    uploadFile

---
+++ NetCore-Backend/Data/FileDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NetCore_Backend.Models;
+
+namespace NetCore_Backend.Data
+{
+    [Table("FileDetails")]
+    public class FileDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+        public FileType FileType { get; set; }
+    }
+}
---
+++ NetCore-Backend/Services/Impl/FileService.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore_Backend.Data;
+using NetCore_Backend.Models;
+
+namespace NetCore_Backend.Services.Impl
+{
+    public class FileService : IFileService
+    {
+        private readonly MyDbContext _context;
+        public FileService(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task PostFileAsync(IFormFile fileData, FileType fileType)
+        {
+            try
+            {
+                var fileDetails = new FileDetails()
+                {
+                    ID = 0,
+                    FileName = fileData.FileName,
+                    FileType = fileType,
+                };
+
+                using (var stream = new MemoryStream())
+                {
+                    fileData.CopyTo(stream);
+                    fileDetails.FileData = stream.ToArray();
+                }
+
+                var result = _context.FileDetails.Add(fileDetails);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task PostMultiFileAsync(List<FileUploadModel> fileData)
+        {
+            try
+            {
+                foreach (FileUploadModel file in fileData)
+                {
+                    var fileDetails = new FileDetails()
+                    {
+                        ID = 0,
+                        FileName = file.FileDetails.FileName,
+                        FileType = file.FileType,
+                    };
+
+                    using (var stream = new MemoryStream())
+                    {
+                        file.FileDetails.CopyTo(stream);
+                        fileDetails.FileData = stream.ToArray();
+                    }
+
+                    var result = _context.FileDetails.Add(fileDetails);
+                }
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task DownloadFileById(int Id)
+        {
+            try
+            {
+                var file = _context.FileDetails.Where(x => x.ID == Id).FirstOrDefaultAsync();
+
+                var content = new System.IO.MemoryStream(file.Result.FileData);
+                var path = Path.Combine(
+                   Directory.GetCurrentDirectory(), "FileDownloaded",
+                   file.Result.FileName);
+
+                await CopyStream(content, path);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task CopyStream(Stream stream, string downloadPath)
+        {
+            using (var fileStream = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
+            {
+                await stream.CopyToAsync(fileStream);
+            }
+        }
+    }
+}
