2e208d2ba6e504e7b35be8407e9c59b3c49cc2c4
TrungBv <trungbui0121@gmail.com>
2023-01-06 18:24:28 +0700

    update

--- NetCore-Backend/Data/Galary.cs
+++ NetCore-Backend/Data/Galary.cs
@@ -15,6 +15,7 @@ namespace NetCore_Backend.Data
         public DateTime ToDate { get; set; }
         public string Authen { get; set; }
         public string Address { get; set; }
+        public string Name { get; set; }
         public string Discription { get; set; }
         public int IsActive { get; set; }
         public DateTime CreeateDate { get; set; }
